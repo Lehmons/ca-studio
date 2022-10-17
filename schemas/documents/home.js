5be1d6864deb7b82564029671e1a8ef4d7f7137c
Lehmons <freddiejackson217@gmail.com>
2022-10-17 14:18:52 +0100

    Sanity studio added

---
+++ schemas/documents/home.js
@@ -0,0 +1,13 @@
+export default {
+  name: "home",
+  title: "Home",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+};
